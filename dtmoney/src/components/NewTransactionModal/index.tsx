8dd6a5f1c8fa4f51f77cf1025be5fb297910e63d
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-03-19 21:35:51 -0300

    chore: Added from

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,30 +1,93 @@
-import React from 'react'
-import Modal from 'react-modal'
-import {Container} from './style'
+import React from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import closeImg from "../../assets/close.svg";
+import Modal from "react-modal";
+import { Container, TransactionTypeContainer, RadioBox } from "./style";
+import {api} from "../../services/api"
 interface NewTransactionModalProps {
-    // Type for props from App.tsx
-    isOpen: boolean;
-    onRequestClose: () => void;
+  // Type for props from App.tsx
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export default function NewTransactionModal({isOpen, onRequestClose,}: NewTransactionModalProps) {
+  const [title, setTitle] = React.useState("");
+  const [value, setValue] = React.useState(0);
+  const [category, setCategory] = React.useState('');
+  const [type, setType] = React.useState("deposit");
+
+  function handleCreateNewTransaction(event: React.FormEvent){
+    event.preventDefault();
+    const data = {
+      title, 
+      value, 
+      category,
+      type
+    }
+    api.post('/transactions', data)
   }
 
-export default function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
   return (
-    <Modal 
-    isOpen={isOpen}
-    onRequestClose={onRequestClose}
-    overlayClassName='react-modal-overlay'
-    className='react-modal-content'
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <Container>
-      <h2>Cadastrar Transação</h2>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="fechar modal" />
+      </button>
 
-      <input placeholder='Titulo' />
-      <input type="number" placeholder='Valor' />
-      <input type="number" placeholder='Categoria' />
-      <button type="submit">Cadastrar</button>
+      <Container onSubmit={handleCreateNewTransaction}>
+        <h2>Cadastrar Transação</h2>
+
+        <input 
+        placeholder="Titulo"
+        value={title}
+        onChange={event => setTitle(event.target.value)} 
+        />
+        <input 
+        type="number" 
+        placeholder="Valor" 
+        value={value}
+        onChange={event => setValue(Number(event.target.value))}
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            className={type === "deposit" ? "active" : ""}
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="income" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="outcome" />
+            <span>Saida</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input 
+        placeholder="Categoria" 
+        value={category}
+        onChange={event => setCategory(event.target.value)} 
+        />
+        <button type="submit">Cadastrar</button>
       </Container>
-      
-
     </Modal>
-  )
+  );
 }
--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,42 +1,55 @@
-import React from 'react'
-import { useEffect } from 'react'
-import {Container} from './styles'
-import {api} from '../../services/api'
+import React from "react";
+import { useEffect } from "react";
+import { Container } from "./styles";
+import { api } from "../../services/api";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
 
-    useEffect(() =>{
-       api.get('transactions')
-            .then(response => console.log(response))
-    },[])
-
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
 
   return (
     <Container>
-        <table>
-            <thead>
-                <th>Titulo</th>
-                <th>Valor</th>
-                <th>Categoria</th>
-                <th>Data</th>
-            </thead>
+      <table>
+        <thead>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
 
-            <tbody>
-                <tr>
-                    <td>Desenvolvimento de sistema</td>
-                    <td className='deposit'>R$12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/02/2022</td>
-                </tr>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className='withdraw'>- R$1.000</td>
-                    <td>Casa</td>
-                    <td>20/02/2022</td>
-                </tr>
-            </tbody>
-        </table>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                    new Date(transaction.createdAt))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
-  )
+  );
 }
