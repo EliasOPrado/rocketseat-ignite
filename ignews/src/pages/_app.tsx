02d37e34573982ab2eb657ede558ef680f932cdd
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-07-24 01:18:00 -0300

    chore: Added Header component

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import { AppProp} from 'next/app'
+import { AppProp} from 'next/app';
+import {Header} from '../components/Header'
+
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProp) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+
+  )
 }
 
 export default MyApp
